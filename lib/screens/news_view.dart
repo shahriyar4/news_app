023bb696f56abc2bad12e07cf70343f3eb399fce
Shahriyar Hasanov <shahrihasan44@gmail.com>
2021-08-26 15:20:34 +0400

    fix clean code

--- lib/screens/news_view.dart
+++ lib/screens/news_view.dart
@@ -12,8 +12,6 @@ class NewsView extends StatefulWidget {
 }
 
 class _NewsViewState extends State<NewsView> {
- 
-
   @override
   Widget build(BuildContext context) {
     return BlocProvider(
@@ -32,107 +30,20 @@ class _NewsViewState extends State<NewsView> {
       body: BlocConsumer<NewsCubit, NewsState>(
         listener: (context, state) {
           if (state is NewsError) {
-            ScaffoldMessenger.of(context)
-                .showSnackBar(SnackBar(content: Text(state.message)));
+            errorSnackbar(context, state);
           }
         },
         builder: (context, state) {
           if (state is NewsInitial) {
             return Center(
-              child: Column(
-                children: [
-                  Text("Hello"),
-                  FloatingActionButton(
-                    onPressed: () => context.read<NewsCubit>().getNews(),
-                    child: Icon(Icons.forward),
-                  )
-                ],
-              ),
+              child: InitialWidget(),
             );
           } else if (state is NewsLoading) {
             return Center(
               child: CircularProgressIndicator(),
             );
           } else if (state is NewsCompleted) {
-            List<NewsModel> _articleList = [];
-            _articleList = state.model;
-            return ListView.builder(
-              itemCount: _articleList.length,
-              itemBuilder: (BuildContext context, int index) {
-                return GestureDetector(
-                  onTap: () async {
-                    if (Platform.isAndroid) {
-                      FlutterWebBrowser.openWebPage(
-                        url: _articleList[index].url.toString(),
-                        customTabsOptions: CustomTabsOptions(
-                          colorScheme: CustomTabsColorScheme.dark,
-                          toolbarColor: Colors.deepPurple,
-                          secondaryToolbarColor: Colors.green,
-                          navigationBarColor: Colors.amber,
-                          addDefaultShareMenuItem: true,
-                          instantAppsEnabled: true,
-                          showTitle: false,
-                          urlBarHidingEnabled: false,
-                        ),
-                      );
-                    }
-                  },
-                  child: Container(
-                    margin: EdgeInsets.all(12.0),
-                    padding: EdgeInsets.all(8.0),
-                    decoration: BoxDecoration(
-                      color: Colors.white,
-                      borderRadius: BorderRadius.circular(12.0),
-                      boxShadow: [
-                        BoxShadow(
-                          color: Colors.black12,
-                          blurRadius: 3.0,
-                        ),
-                      ],
-                    ),
-                    child: Column(
-                      mainAxisAlignment: MainAxisAlignment.start,
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: [
-                        Container(
-                          height: 200.0,
-                          width: double.infinity,
-                          decoration: BoxDecoration(
-                            image: DecorationImage(
-                                image: NetworkImage(
-                                  _articleList[index].urlToImage != null
-                                      ? _articleList[index]
-                                          .urlToImage
-                                          .toString()
-                                      : "https://i.stack.imgur.com/y9DpT.jpg",
-                                ),
-                                fit: BoxFit.cover),
-                            borderRadius: BorderRadius.circular(12.0),
-                          ),
-                        ),
-                        SizedBox(
-                          height: 8.0,
-                        ),
-                        SizedBox(
-                          height: 8.0,
-                        ),
-                        Wrap(
-                          children: [
-                            Text(
-                              _articleList[index].title.toString(),
-                              style: TextStyle(
-                                fontWeight: FontWeight.bold,
-                                fontSize: 16.0,
-                              ),
-                            ),
-                          ],
-                        ),
-                      ],
-                    ),
-                  ),
-                );
-              },
-            );
+            return completedListBuilder(state);
           } else {
             final error = state as NewsError;
             return Center(
@@ -141,6 +52,132 @@ class _NewsViewState extends State<NewsView> {
           }
         },
       ),
+    );
+  }
+
+  ListView completedListBuilder(NewsCompleted state) {
+    List<NewsModel> _articleList = [];
+    _articleList = state.model;
+    return ListView.builder(
+      itemCount: _articleList.length,
+      itemBuilder: (BuildContext context, int index) {
+        return buildCompletedGestureDedector(_articleList, index);
+      },
+    );
+  }
+
+  void errorSnackbar(BuildContext context, NewsError state) {
+    ScaffoldMessenger.of(context)
+        .showSnackBar(SnackBar(content: Text(state.message)));
+  }
+
+  GestureDetector buildCompletedGestureDedector(
+      List<NewsModel> _articleList, int index) {
+    return GestureDetector(
+      onTap: () async {
+        if (Platform.isAndroid) {
+          buildWebBrowser(_articleList, index);
+        }
+      },
+      child: buildCompletedContainer(_articleList, index),
+    );
+  }
+
+  Container buildCompletedContainer(List<NewsModel> _articleList, int index) {
+    return Container(
+      margin: EdgeInsets.all(12.0),
+      padding: EdgeInsets.all(8.0),
+      decoration: BoxDecoration(
+        color: Colors.white,
+        borderRadius: BorderRadius.circular(12.0),
+        boxShadow: [
+          BoxShadow(
+            color: Colors.black12,
+            blurRadius: 3.0,
+          ),
+        ],
+      ),
+      child: buildNewsColumn(_articleList, index),
+    );
+  }
+
+  Column buildNewsColumn(List<NewsModel> _articleList, int index) {
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.start,
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        buildImageContiner(_articleList, index),
+        SizedBox(
+          height: 8.0,
+        ),
+        Wrap(
+          children: [
+            buildTitleText(_articleList, index),
+          ],
+        ),
+      ],
+    );
+  }
+
+  Text buildTitleText(List<NewsModel> _articleList, int index) {
+    return Text(
+      _articleList[index].title.toString(),
+      style: TextStyle(
+        fontWeight: FontWeight.bold,
+        fontSize: 16.0,
+      ),
+    );
+  }
+
+  Container buildImageContiner(List<NewsModel> _articleList, int index) {
+    return Container(
+      height: 200.0,
+      width: double.infinity,
+      decoration: BoxDecoration(
+        image: DecorationImage(
+            image: NetworkImage(
+              _articleList[index].urlToImage != null
+                  ? _articleList[index].urlToImage.toString()
+                  : "https://i.stack.imgur.com/y9DpT.jpg",
+            ),
+            fit: BoxFit.cover),
+        borderRadius: BorderRadius.circular(12.0),
+      ),
+    );
+  }
+
+  void buildWebBrowser(List<NewsModel> _articleList, int index) {
+    FlutterWebBrowser.openWebPage(
+      url: _articleList[index].url.toString(),
+      customTabsOptions: CustomTabsOptions(
+        colorScheme: CustomTabsColorScheme.dark,
+        toolbarColor: Colors.deepPurple,
+        secondaryToolbarColor: Colors.green,
+        navigationBarColor: Colors.amber,
+        addDefaultShareMenuItem: true,
+        instantAppsEnabled: true,
+        showTitle: false,
+        urlBarHidingEnabled: false,
+      ),
+    );
+  }
+}
+
+class InitialWidget extends StatelessWidget {
+  const InitialWidget({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        Text("Hello"),
+        FloatingActionButton(
+          onPressed: () => context.read<NewsCubit>().getNews(),
+          child: Icon(Icons.forward),
+        )
+      ],
     );
   }
 }
